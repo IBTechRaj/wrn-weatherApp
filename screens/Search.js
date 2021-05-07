1b25acdeff291c19f1df6392450b7aa46af4be7a
IBTechRaj <raj_shk@rediffmail.com>
2021-05-07 18:11:13 +0530

    weather app header done

---
+++ screens/Search.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Appbar, Title } from 'react-native-paper';
+import { View, Text } from 'react-native'
+import { Header } from './Header'
+
+export default Search = (props) => {
+
+  return (
+    <View>
+      <Text> Search Screen</Text>
+    </View>
+  );
+
+}
